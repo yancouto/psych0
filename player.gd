9a56a98645eacac48ff5d8f0d0bdb481f2cb6503
Yan Couto <yancouto@gmail.com>
2023-10-03 21:16:59 -0300

    Player can't leave screen
    Closes #2

--- player.gd
+++ player.gd
@@ -2,12 +2,16 @@ extends Area2D
 signal hit
 
 @export var speed := 400
+@export var radius := 20
 var screen_size
 
+var CLAMP_LEFT := Vector2(radius, radius)
+var CLAMP_RIGHT := Vector2(LevelBuilder.W - radius, LevelBuilder.H - radius)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = screen_size / 2
+	($CollisionShape2D.shape as CircleShape2D).radius = radius
 
 func _process(delta):
 	var vel = Vector2.ZERO
@@ -19,7 +23,9 @@ func _process(delta):
 		vel.x -= 1
 	if Input.is_action_pressed("move_right"):
 		vel.x += 1
-	position += vel.normalized() * speed * delta
+	if !vel.is_zero_approx():
+		position += vel.normalized() * speed * delta
+		position = position.clamp(CLAMP_LEFT, CLAMP_RIGHT)
 
 func start(pos):
 	position = pos
